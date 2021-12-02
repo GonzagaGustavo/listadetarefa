b18f969b45066b9c718b32595c05eda6c810179b
Gustavo Gonzaga <94642849+Gustavo13132@users.noreply.github.com>
2021-12-02 13:11:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const lista = document.getElementById("lista")
 const tarefa = []
 
 function add() {
+    if (input.value != "")
     tarefa.push(input.value)
     input.value = ""
     render()
@@ -14,4 +15,33 @@ function render() {
         li.innerText = i
         lista.appendChild(li)
     })
+}
+removeTask = function( itemId ){
+
+    for( i = 0 ; i < ul.children.length ; i++){
+
+        if(ul.children[i].getAttribute("index") == itemId ){
+
+            ul.children[i].remove();
+
+        }
+    }
+}
+
+createItemEl = function(itemValue, itemId){
+
+    let li = document.createElement("li");
+
+    li.setAttribute("index", itemId);
+
+    li.appendChild(document.createTextNode(itemValue));
+
+    return li;
+}
+
+createRemoveTaskBtn = function(itemId){
+    let btn =  document.createElement("button");
+    btn.setAttribute("onclick", "removeTask("+itemId+")"); 
+    btn.innerHTML ="X";
+    return btn;
 }
